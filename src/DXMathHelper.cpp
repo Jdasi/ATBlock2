18a419d492fea4d4f8555c256100c305d970b913
Joe da Silva <silvacore@hotmail.co.uk>
2017-12-17 18:48:32 +0000

    tiny corrections/changes to DXMathHelper

--- src/DXMathHelper.cpp
+++ src/DXMathHelper.cpp
@@ -4,103 +4,85 @@
 
 DirectX::XMFLOAT3 DirectX::Float3Add(const XMFLOAT3& _a, const XMFLOAT3& _b)
 {
-    DirectX::XMFLOAT3 f;
+    float x = _a.x + _b.x;
+    float y = _a.y + _b.y;
+    float z = _a.z + _b.z;
 
-    f.x = _a.x + _b.x;
-    f.y = _a.y + _b.y;
-    f.z = _a.z + _b.z;
-
-    return f;
+    return DirectX::XMFLOAT3{ x, y , z };
 }
 
 
 DirectX::XMFLOAT3 DirectX::Float3SubtractBfromA(const XMFLOAT3& _a, const XMFLOAT3& _b)
 {
-    DirectX::XMFLOAT3 f;
+    float x = _a.x - _b.x;
+    float y = _a.y - _b.y;
+    float z = _a.z - _b.z;
 
-    f.x = _a.x - _b.x;
-    f.y = _a.y - _b.y;
-    f.z = _a.z - _b.z;
-
-    return f;
+    return DirectX::XMFLOAT3{ x, y , z };
 }
 
 
 DirectX::XMFLOAT3 DirectX::Float3Mul(const XMFLOAT3& _a, const int _mul)
 {
-    DirectX::XMFLOAT3 f;
+    float x = _a.x * _mul;
+    float y = _a.y * _mul;
+    float z = _a.z * _mul;
 
-    f.x = _a.x * _mul;
-    f.y = _a.y * _mul;
-    f.z = _a.z * _mul;
-
-    return f;
+    return DirectX::XMFLOAT3{ x, y , z };
 }
 
 
 DirectX::XMFLOAT3 DirectX::Float3Mul(const XMFLOAT3& _a, const float _mul)
 {
-    DirectX::XMFLOAT3 f;
+    float x = _a.x * _mul;
+    float y = _a.y * _mul;
+    float z = _a.z * _mul;
 
-    f.x = _a.x * _mul;
-    f.y = _a.y * _mul;
-    f.z = _a.z * _mul;
-
-    return f;
+    return DirectX::XMFLOAT3{ x, y , z };
 }
 
 
 DirectX::XMFLOAT3 DirectX::Float3Div(const XMFLOAT3& _a, const int _div)
 {
-    DirectX::XMFLOAT3 f;
+    float x = _a.x / _div;
+    float y = _a.y / _div;
+    float z = _a.z / _div;
 
-    f.x = _a.x / _div;
-    f.y = _a.y / _div;
-    f.z = _a.z / _div;
-
-    return f;
+    return DirectX::XMFLOAT3{ x, y , z };
 }
 
 
 DirectX::XMFLOAT3 DirectX::Float3Div(const XMFLOAT3& _a, const float _div)
 {
-    DirectX::XMFLOAT3 f;
+    float x = _a.x / _div;
+    float y = _a.y / _div;
+    float z = _a.z / _div;
 
-    f.x = _a.x / _div;
-    f.y = _a.y / _div;
-    f.z = _a.z / _div;
-
-    return f;
+    return DirectX::XMFLOAT3{ x, y , z };
 }
 
 
 DirectX::XMFLOAT3 DirectX::Float3Normalized(const XMFLOAT3& _a)
 {
-    DirectX::XMFLOAT3 f;
+    float mag = JHelper::fisqrt(Float3MagnitudeSquared(_a));
 
-    float mag_sqr = Float3MagnitudeSquared(_a);
-    float inv_sqrt = JHelper::fisqrt(mag_sqr);
+    float x = _a.x * mag;
+    float y = _a.y * mag;
+    float z = _a.z * mag;
 
-    f.x = _a.x * inv_sqrt;
-    f.y = _a.y * inv_sqrt;
-    f.z = _a.z * inv_sqrt;
-
-    return f;
+    return DirectX::XMFLOAT3{ x, y , z };
 }
 
 
 DirectX::XMFLOAT3 DirectX::Float3DirectionAtoB(const XMFLOAT3& _a, const XMFLOAT3& _b)
 {
-    DirectX::XMFLOAT3 sub = Float3SubtractBfromA(_b, _a);
-    sub = DirectX::Float3Normalized(sub);
-
-    return sub;
+    return DirectX::Float3Normalized(Float3SubtractBfromA(_b, _a));
 }
 
 
 float DirectX::Float3Magnitude(const XMFLOAT3& _a)
 {
-    return 1 / JHelper::fisqrt(_a.x * _a.x + _a.y * _a.y + _a.z * _a.z);
+    return JHelper::fisqrt(_a.x * _a.x + _a.y * _a.y + _a.z * _a.z);
 }
 
 
@@ -113,7 +95,7 @@ float DirectX::Float3MagnitudeSquared(const XMFLOAT3& _a)
 float DirectX::Float3Distance(const XMFLOAT3& _a, const XMFLOAT3& _b)
 {
     XMFLOAT3 diff = Float3SubtractBfromA(_a, _b);
-    return 1 / JHelper::fisqrt((diff.x * diff.x) + (diff.y * diff.y) + (diff.z * diff.z));
+    return JHelper::fisqrt((diff.x * diff.x) + (diff.y * diff.y) + (diff.z * diff.z));
 }
 
 
